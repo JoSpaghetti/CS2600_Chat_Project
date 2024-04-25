ed1450492171850761c86b995f621b12aaa2b258
Jairus M. Legion <jmlegion@login-01.unx.cpp.edu>
2024-04-24 18:18:13 -0700

    updated chatclient

--- chat_client.c
+++ chat_client.c
@@ -1,2 +1,23 @@
+<<<<<<< Updated upstream
 #include <stdio.h>
 
+=======
+#include <sys.h>
+#include <socket.h>i
+
+int main() {
+  //fd for the socket
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) report ("socket", 1);
+
+  //gets the address of the host
+  struct hostent* hptr = gethostbyname("127.0.0.1");
+  if (!htpr) report ("gethostbyname", 1);
+  if (htpr->h_addrtype != AF_INET)
+    report("bad address family", 1);
+
+  struct socaddr_in saddr;
+  memset(&saddr, 0, sizeof(saddr)
+  
+}
+>>>>>>> Stashed changes
